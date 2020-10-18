89a884e31a09bf64164c28abc6b315679735b415
Mau Abata <mauabata@gmail.com>
2020-10-18 18:44:04 -0500

    Fix missing SD card error.

---
+++ errors.h
@@ -0,0 +1,19 @@
+#ifndef __errors_h
+#define __errors_h
+
+/**
+ * Failed to rename existing configuration.
+ */
+#define E_SAV_BAK "E_SAV_BAK"
+
+/**
+ * Failed to create new file for settings save.
+ */
+#define E_SAV_TMP "E_SAV_TMP"
+
+/**
+ * Failed to serialize configuration to JSON.
+ */
+#define E_SAV_SER "E_SAV_SER"
+
+#endif
