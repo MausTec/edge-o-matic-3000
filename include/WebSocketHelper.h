42054e1ab9d402eb4572b48189e61456ad6db8b4
Mau Abata <mauabata@gmail.com>
2020-10-29 17:42:14 -0500

    Dispatch incoming WebSocket messages. Implementation required.

--- include/WebSocketHelper.h
+++ include/WebSocketHelper.h
@@ -2,11 +2,24 @@
 #define __WebSocketHelper_h
 
 #include <WebSocketsServer.h>
+#include <map>
 
+#define ARDUINOJSON_USE_LONG_LONG 1
+#include <ArduinoJson.h>
+
+typedef struct WebSocketConnection {
+  int num;
+  IPAddress ip;
+  bool stream_readings = true;
+  bool stream_screen_data = false;
+} WebSocketConnection;
 
 namespace WebSocketHelper {
   void begin();
   void tick();
+
+  void send(const char *cmd, JsonDocument &doc, int num = -1);
+  void send(const char *cmd, String text, int num);
 
   void sendSettings(int num = -1);
   void sendWxStatus(int num = -1);
@@ -14,10 +27,11 @@ namespace WebSocketHelper {
   void sendReadings(int num = -1);
 
   namespace {
-    WebSocketsServer* webSocket; // This is now a pointer, because apparently there is no default constructor and
-    // it *must* be initialized with a port, which we don't know until config load.
+    WebSocketsServer* webSocket;
     uint8_t last_connection;
     bool stream_data = false;
+
+    std::map<int, WebSocketConnection*> connections;
 
     void onMessage(int num, uint8_t * payload);
 
