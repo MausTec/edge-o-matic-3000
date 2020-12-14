cd02c71a67a92ea0caef3952427e174773a21f60
Mau Abata <mauabata@gmail.com>
2020-12-13 20:44:38 -0600

    WebSocket helpers now use the WebSocketSecure server. This is a huge performance drag, and I already ran out of heap space. What is this, an Atmel?

--- include/WebSocketSecureHelper.h
+++ include/WebSocketSecureHelper.h
@@ -1,4 +1,63 @@
+#include <sstream>
+
+// Max clients to be connected to the ESP32
+#define MAX_CLIENTS 2
+
+// We will use wifi
+#include <WiFi.h>
+
+// Includes for the server
+#include <HTTPSServer.hpp>
+#include <SSLCert.hpp>
+#include <HTTPRequest.hpp>
+#include <HTTPResponse.hpp>
+#include <WebsocketHandler.hpp>
+
+
+// The HTTPS Server comes in a separate namespace. For easier use, include it here.
+using namespace httpsserver;
+
 namespace WebSocketSecureHelper {
   void setup();
   void loop();
+
+  // Send message to a specific client or all clients.
+  void send(int num, String msg);
+
+  // Declare some handler functions for the various URLs on the server
+  // The signature is always the same for those functions. They get two parameters,
+  // which are pointers to the request data (read request body, headers, ...) and
+  // to the response data (write response, set status code, ...)
+  void handleRoot(HTTPRequest *req, HTTPResponse *res);
+  void handle404(HTTPRequest *req, HTTPResponse *res);
+  void handleCors(HTTPRequest *req, HTTPResponse *res);
+
+  // As websockets are more complex, they need a custom class that is derived from WebsocketHandler
+  class RemoteHandler : public WebsocketHandler {
+  public:
+    // This method is called by the webserver to instantiate a new handler for each
+    // client that connects to the websocket endpoint
+    static WebsocketHandler *create();
+
+    // This method is called when a message arrives
+    void onMessage(WebsocketInputStreambuf *input);
+
+    // Handler function on connection close
+    void onClose();
+
+    // Gets the current index
+    int getIndex();
+
+    // Sends text
+    void sendText(String payload);
+  };
+
+
+  namespace {
+    SSLCert *cert = nullptr;
+    HTTPSServer *secureServer = nullptr;
+
+    // Simple array to store the active clients:
+    WebsocketHandler *activeClients[MAX_CLIENTS];
+  }
 }
--- src/WebSocketSecureHelper.cpp
+++ src/WebSocketSecureHelper.cpp
@@ -1,115 +1,51 @@
-/**
- * Example for the ESP32 HTTP(S) Webserver
- *
- * IMPORTANT NOTE:
- * To run this script, you need to
- *  1) Enter your WiFi SSID and PSK below this comment
- *  2) Make sure to have certificate data available. You will find a
- *     shell script and instructions to do so in the library folder
- *     under extras/
- *
- * This script will install an HTTPS Server on your ESP32 with the following
- * functionalities:
- *  - Show a chat interface on the root node /
- *  - Use a websocket to allow multiple clients to pass messages to each other
- */
+#include "../include/WebSocketSecureHelper.h"
+#include "../include/WebSocketHelper.h"
 
-#include <sstream>
-
-// Max clients to be connected to the chat
-#define MAX_CLIENTS 4
-
-// Include certificate data (see note above)
+// Include certificate data
 #include "../include/ssl/cert.h"
 #include "../include/ssl/private_key.h"
 
-// We will use wifi
-#include <WiFi.h>
-
-// Includes for the server
-#include <HTTPSServer.hpp>
-#include <SSLCert.hpp>
-#include <HTTPRequest.hpp>
-#include <HTTPResponse.hpp>
-#include <WebsocketHandler.hpp>
-
-// The HTTPS Server comes in a separate namespace. For easier use, include it here.
-using namespace httpsserver;
-
 namespace WebSocketSecureHelper {
-  // Create an SSL certificate object from the files included above ...
-  SSLCert cert = SSLCert(
-      example_crt_DER, example_crt_DER_len,
-      example_key_DER, example_key_DER_len
-  );
-  // ... and create a server based on this certificate.
-  // The constructor has some optional parameters like the TCP port that should be used
-  // and the max client count. For simplicity, we use a fixed amount of clients that is bound
-  // to the max client count.
-  HTTPSServer secureServer = HTTPSServer(&cert, 443, MAX_CLIENTS);
-
-  // Declare some handler functions for the various URLs on the server
-  // The signature is always the same for those functions. They get two parameters,
-  // which are pointers to the request data (read request body, headers, ...) and
-  // to the response data (write response, set status code, ...)
-  void handleRoot(HTTPRequest *req, HTTPResponse *res);
-
-  void handle404(HTTPRequest *req, HTTPResponse *res);
-
-  // As websockets are more complex, they need a custom class that is derived from WebsocketHandler
-  class ChatHandler : public WebsocketHandler {
-  public:
-    // This method is called by the webserver to instantiate a new handler for each
-    // client that connects to the websocket endpoint
-    static WebsocketHandler *create();
-
-    // This method is called when a message arrives
-    void onMessage(WebsocketInputStreambuf *input);
-
-    // Handler function on connection close
-    void onClose();
-  };
-
-  // Simple array to store the active clients:
-  ChatHandler *activeClients[MAX_CLIENTS];
-
-  void corsCallback(HTTPRequest *req, HTTPResponse *res) {
-    Serial.println(">> CORS here...");
-    res->setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res->setHeader("Access-Control-Allow-Origin", "*");
-    res->setHeader("Access-Control-Allow-Headers", "*");
-  }
-
   void setup() {
+    // Create an SSL certificate object from the files included above ...
+    cert = new SSLCert(
+        example_crt_DER, example_crt_DER_len,
+        example_key_DER, example_key_DER_len
+    );
+
+    // ... and create a server based on this certificate.
+    // The constructor has some optional parameters like the TCP port that should be used
+    // and the max client count. For simplicity, we use a fixed amount of clients that is bound
+    // to the max client count.
+    secureServer = new HTTPSServer(cert, 443, MAX_CLIENTS);
+
     // Initialize the slots
     for (int i = 0; i < MAX_CLIENTS; i++) activeClients[i] = nullptr;
 
     // For every resource available on the server, we need to create a ResourceNode
     // The ResourceNode links URL and HTTP method to a handler function
     ResourceNode *nodeRoot = new ResourceNode("/", "GET", &handleRoot);
-    ResourceNode *node404 = new ResourceNode("", "GET", &handle404);
+    secureServer->registerNode(nodeRoot);
 
-    // Add the root node to the server
-    secureServer.registerNode(nodeRoot);
-
-    ResourceNode *corsNode = new ResourceNode("/*", "OPTIONS", &corsCallback);
-    secureServer.registerNode(corsNode);
+    // Check CORS here. Some browsers appreciate proper preflights before connecting
+    // to WebSockets. Security and all, yanno?
+    ResourceNode *corsNode = new ResourceNode("/*", "OPTIONS", &handleCors);
+    secureServer->registerNode(corsNode);
 
     // The websocket handler can be linked to the server by using a WebsocketNode:
     // (Note that the standard defines GET as the only allowed method here,
     // so you do not need to pass it explicitly)
-    WebsocketNode *chatNode = new WebsocketNode("/", &ChatHandler::create);
-
-    // Adding the node to the server works in the same way as for all other nodes
-    secureServer.registerNode(chatNode);
+    WebsocketNode *chatNode = new WebsocketNode("/", &RemoteHandler::create);
+    secureServer->registerNode(chatNode);
 
     // Finally, add the 404 not found node to the server.
     // The path is ignored for the default node.
-    secureServer.setDefaultNode(node404);
+    ResourceNode *node404 = new ResourceNode("", "GET", &handle404);
+    secureServer->setDefaultNode(node404);
 
     Serial.println("Starting server...");
-    secureServer.start();
-    if (secureServer.isRunning()) {
+    secureServer->start();
+    if (secureServer->isRunning()) {
       Serial.print("Server ready. Open the following URL in multiple browser windows to start chatting: https://");
       Serial.println(WiFi.localIP());
     }
@@ -117,7 +53,19 @@ namespace WebSocketSecureHelper {
 
   void loop() {
     // This call will let the server do its work
-    secureServer.loop();
+    secureServer->loop();
+  }
+
+  void send(int num, String payload) {
+    if (num >= 0) {
+      RemoteHandler *c = (RemoteHandler*) activeClients[num];
+      if (c != nullptr)
+        c->sendText(payload);
+    } else {
+      for (int i = 0; i < MAX_CLIENTS; i++) {
+        send(i, payload);
+      }
+    }
   }
 
   void handle404(HTTPRequest *req, HTTPResponse *res) {
@@ -140,11 +88,32 @@ namespace WebSocketSecureHelper {
     res->println("</html>");
   }
 
+  // The following HTML code will present the chat interface.
+  void handleRoot(HTTPRequest *req, HTTPResponse *res) {
+    res->setHeader("Content-Type", "text/html");
+
+    // TODO Redirect here
+
+    res->println("<!DOCTYPE html>");
+    res->println("<html>");
+    res->println("<head><title>Redirecting...</title></head>");
+    res->println("<body><h1>Redirecting...</h1><p>You are now being redirected to the Web UI for the Edge-o-Matic.</p></body>");
+    res->println("</html>");
+  }
+
+  void handleCors(HTTPRequest *req, HTTPResponse *res) {
+    Serial.println(">> CORS here...");
+    res->setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res->setHeader("Access-Control-Allow-Origin", "*");
+    res->setHeader("Access-Control-Allow-Headers", "*");
+  }
+
+
   // In the create function of the handler, we create a new Handler and keep track
   // of it using the activeClients array
-  WebsocketHandler *ChatHandler::create() {
+  WebsocketHandler *RemoteHandler::create() {
     Serial.println("Creating new chat client!");
-    ChatHandler *handler = new ChatHandler();
+    WebsocketHandler *handler = new RemoteHandler();
     for (int i = 0; i < MAX_CLIENTS; i++) {
       if (activeClients[i] == nullptr) {
         activeClients[i] = handler;
@@ -154,8 +123,17 @@ namespace WebSocketSecureHelper {
     return handler;
   }
 
+  int RemoteHandler::getIndex() {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+      if (activeClients[i] == this) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
   // When the websocket is closing, we remove the client from the array
-  void ChatHandler::onClose() {
+  void RemoteHandler::onClose() {
     for (int i = 0; i < MAX_CLIENTS; i++) {
       if (activeClients[i] == this) {
         activeClients[i] = nullptr;
@@ -165,121 +143,17 @@ namespace WebSocketSecureHelper {
 
   // Finally, passing messages around. If we receive something, we send it to all
   // other clients
-  void ChatHandler::onMessage(WebsocketInputStreambuf *inbuf) {
+  void RemoteHandler::onMessage(WebsocketInputStreambuf *inbuf) {
     // Get the input message
     std::ostringstream ss;
     std::string msg;
     ss << inbuf;
     msg = ss.str();
 
-    // Send it back to every client
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-      if (activeClients[i] != nullptr) {
-        activeClients[i]->send(msg, SEND_TYPE_TEXT);
-      }
-    }
+    WebSocketHelper::onMessage(this->getIndex(), msg.c_str());
   }
 
-
-  // The following HTML code will present the chat interface.
-  void handleRoot(HTTPRequest *req, HTTPResponse *res) {
-    res->setHeader("Content-Type", "text/html");
-
-    res->print(
-        "<!DOCTYPE HTML>\n"
-        "<html>\n"
-        "   <head>\n"
-        "   <title>ESP32 Chat</title>\n"
-        "</head>\n"
-        "<body>\n"
-        "    <div style=\"width:500px;border:1px solid black;margin:20px auto;display:block\">\n"
-        "        <form onsubmit=\"return false\">\n"
-        "            Your Name: <input type=\"text\" id=\"txtName\" value=\"ESP32 user\">\n"
-        "            <button type=\"submit\" id=\"btnConnect\">Connect</button>\n"
-        "        </form>\n"
-        "        <form onsubmit=\"return false\">\n"
-        "            <div style=\"overflow:scroll;height:400px\" id=\"divOut\">Not connected...</div>\n"
-        "            Your Message: <input type=\"text\" id=\"txtChat\" disabled>\n"
-        "            <button type=\"submit\" id=\"btnSend\" disabled>Send</button>\n"
-        "        </form>\n"
-        "    </div>\n"
-        "    <script type=\"text/javascript\">\n"
-        "        const elem = id => document.getElementById(id);\n"
-        "        const txtName = elem(\"txtName\");\n"
-        "        const txtChat = elem(\"txtChat\");\n"
-        "        const btnConnect = elem(\"btnConnect\");\n"
-        "        const btnSend = elem(\"btnSend\");\n"
-        "        const divOut = elem(\"divOut\");\n"
-        "\n"
-        "        class Chat {\n"
-        "            constructor() {\n"
-        "                this.connecting = false;\n"
-        "                this.connected = false;\n"
-        "                this.name = \"\";\n"
-        "                this.ws = null;\n"
-        "            }\n"
-        "            connect() {\n"
-        "                if (this.ws === null) {\n"
-        "                    this.connecting = true;\n"
-        "                    txtName.disabled = true;\n"
-        "                    this.name = txtName.value;\n"
-        "                    btnConnect.innerHTML = \"Connecting...\";\n"
-        "                    this.ws = new WebSocket(\"wss://\" + document.location.host + \"/chat\");\n"
-        "                    this.ws.onopen = e => {\n"
-        "                        this.connecting = false;\n"
-        "                        this.connected = true;\n"
-        "                        divOut.innerHTML = \"<p>Connected.</p>\";\n"
-        "                        btnConnect.innerHTML = \"Disconnect\";\n"
-        "                        txtChat.disabled=false;\n"
-        "                        btnSend.disabled=false;\n"
-        "                        this.ws.send(this.name + \" joined!\");\n"
-        "                    };\n"
-        "                    this.ws.onmessage = e => {\n"
-        "                        divOut.innerHTML+=\"<p>\"+e.data+\"</p>\";\n"
-        "                        divOut.scrollTo(0,divOut.scrollHeight);\n"
-        "                    }\n"
-        "                    this.ws.onclose = e => {\n"
-        "                        this.disconnect();\n"
-        "                    }\n"
-        "                }\n"
-        "            }\n"
-        "            disconnect() {\n"
-        "                if (this.ws !== null) {\n"
-        "                    this.ws.send(this.name + \" left!\");\n"
-        "                    this.ws.close();\n"
-        "                    this.ws = null;\n"
-        "                }\n"
-        "                if (this.connected) {\n"
-        "                    this.connected = false;\n"
-        "                    txtChat.disabled=true;\n"
-        "                    btnSend.disabled=true;\n"
-        "                    txtName.disabled = false;\n"
-        "                    divOut.innerHTML+=\"<p>Disconnected.</p>\";\n"
-        "                    btnConnect.innerHTML = \"Connect\";\n"
-        "                }\n"
-        "            }\n"
-        "            sendMessage(msg) {\n"
-        "                if (this.ws !== null) {\n"
-        "                    this.ws.send(this.name + \": \" + msg);\n"
-        "                }\n"
-        "            }\n"
-        "        };\n"
-        "        let chat = new Chat();\n"
-        "        btnConnect.onclick = () => {\n"
-        "            if (chat.connected) {\n"
-        "                chat.disconnect();\n"
-        "            } else if (!chat.connected && !chat.connecting) {\n"
-        "                chat.connect();\n"
-        "            }\n"
-        "        }\n"
-        "        btnSend.onclick = () => {\n"
-        "            chat.sendMessage(txtChat.value);\n"
-        "            txtChat.value=\"\";\n"
-        "            txtChat.focus();\n"
-        "        }\n"
-        "    </script>\n"
-        "</body>\n"
-        "</html>\n"
-    );
+  void RemoteHandler::sendText(String payload) {
+    this->send(payload.c_str(), SEND_TYPE_TEXT);
   }
 }
