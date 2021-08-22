b4ab6add539353afe48eba38e18d6daa5ed51e8e
Mau Abata <69772612+MauAbata@users.noreply.github.com>
2021-08-22 11:20:31 -0500

    Create VERSION.h

---
+++ include/VERSION.h
@@ -0,0 +1,8 @@
+/**
+ * This file will be automatically generated for production builds, and should
+ * only be modified if you need to for whatever thing you're testing (auto
+ * updates or whatnot?). Do not commit your changes to this file. It's ignored.
+ *
+ * To figure out what version you're actually using, check your tags. Thanks!
+ */
+#define VERSION "99.0.0"
