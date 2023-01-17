c35c9adeb78baa92458bb28dff1775d121aee395
Mau Abata <mauabata@gmail.com>
2023-01-17 17:35:48 -0600

    Skeleton for new UI code.

---
+++ include/ui/input.h
@@ -0,0 +1,14 @@
+#ifndef __ui__input_h
+#define __ui__input_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/ui/menu.h
@@ -0,0 +1,14 @@
+#ifndef __ui__menu_h
+#define __ui__menu_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
