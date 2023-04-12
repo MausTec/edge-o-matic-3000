1a34f2e8f87fec8f04800b8b8e9433835895341d
Mau Abata <mauabata@gmail.com>
2023-04-12 16:33:34 -0500

    Update some remote deps.

--- src/accessory_driver.c
+++ src/accessory_driver.c
@@ -1,19 +1,20 @@
-#include "accessory_port.h"
-#include "maus_bus.h"
 #include "eom-hal.h"
 #include "esp_log.h"
+#include "maus_bus.h"
 #include "tscode.h"
 #include <string.h>
 
-static const char *TAG = "accessory_driver";
+static const char* TAG = "accessory_driver";
 
-static maus_bus_err_t _accessory_read(uint8_t address, uint8_t subaddress, uint8_t *data, size_t len) {
+static maus_bus_err_t
+_accessory_read(uint8_t address, uint8_t subaddress, uint8_t* data, size_t len) {
     maus_bus_err_t err = MAUS_BUS_OK;
     eom_hal_accessory_master_read_register(address, subaddress, data, len);
     return err;
 }
 
-static maus_bus_err_t _accessory_write(uint8_t address, uint8_t subaddress, uint8_t *data, size_t len) {
+static maus_bus_err_t
+_accessory_write(uint8_t address, uint8_t subaddress, uint8_t* data, size_t len) {
     maus_bus_err_t err = MAUS_BUS_OK;
     eom_hal_accessory_master_write_register(address, subaddress, data, len);
     return err;
@@ -28,8 +29,10 @@ void accessory_driver_init(void) {
     maus_bus_init(&config);
 }
 
-static void _device_speed_cb(maus_bus_driver_t *driver, maus_bus_device_t *device, maus_bus_address_t address, void *ptr) {
-    uint8_t speed = *((uint8_t*) ptr);
+static void _device_speed_cb(
+    maus_bus_driver_t* driver, maus_bus_device_t* device, maus_bus_address_t address, void* ptr
+) {
+    uint8_t speed = *((uint8_t*)ptr);
 
     if (driver->transmit != NULL) {
         if (device->features.tscode) {
@@ -37,7 +40,7 @@ static void _device_speed_cb(maus_bus_driver_t *driver, maus_bus_device_t *devic
 
             tscode_command_t cmd = TSCODE_COMMAND_DEFAULT;
             cmd.type = speed > 0 ? TSCODE_RECIPROCATING_MOVE : TSCODE_CONDITIONAL_STOP;
-            cmd.speed = (tscode_unit_t *) malloc(sizeof(tscode_unit_t));
+            cmd.speed = (tscode_unit_t*)malloc(sizeof(tscode_unit_t));
             cmd.speed->unit = TSCODE_UNIT_BYTE;
             cmd.speed->value = speed;
             ESP_LOGI(TAG, " -> TSCODE Device: %s", device->product_name);
@@ -48,7 +51,7 @@ static void _device_speed_cb(maus_bus_driver_t *driver, maus_bus_device_t *devic
             ESP_LOGI(TAG, "    -> Serialized: %s", buffer);
             tscode_dispose_command(&cmd);
 
-            driver->transmit((uint8_t*) buffer, strlen(buffer));
+            driver->transmit((uint8_t*)buffer, strlen(buffer));
         }
     }
 }
