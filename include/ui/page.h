033f72fdf7cc20fdba74ae0951acadabd9b87632
Mau Abata <mauabata@gmail.com>
2023-04-27 16:39:16 -0500

    Splash screen.

--- include/ui/page.h
+++ include/ui/page.h
@@ -52,6 +52,7 @@ void ui_page_handle_close(const ui_page_t* p, void* arg);
 extern const ui_page_t PAGE_SNAKE;
 extern const ui_page_t PAGE_EDGING_STATS;
 extern const ui_page_t PAGE_APP_RUNNER;
+extern const ui_page_t SPLASH_PAGE;
 
 #ifdef __cplusplus
 }
