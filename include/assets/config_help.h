6b0e6f062d0d3e8f5063d5f1a40245649adfbebc
Mau Abata <mauabata@gmail.com>
2023-05-02 14:10:12 -0500

    Fixed help texts.

--- include/assets/config_help.h
+++ include/assets/config_help.h
@@ -21,7 +21,7 @@ extern "C" {
 #define _HELPSTR(str) NULL
 #endif
 
-#define WIFI_SSID_HELP _HELPSTR("Your WiFi SSID")
+#define WIFI_SSID_HELP _HELPSTR("Your WiFi SSID.")
 #define WIFI_KEY_HELP _HELPSTR("Your WiFi Password.")
 #define WIFI_ON_HELP _HELPSTR("True to enable WiFi / Websocket server.")
 #define BT_DISPLAY_NAME_HELP _HELPSTR("AzureFang* device name, you might wanna change this.")
@@ -33,7 +33,7 @@ extern "C" {
 #define HOSTNAME_HELP _HELPSTR("Local hostname for your device.")
 #define MOTOR_START_SPEED_HELP _HELPSTR("The minimum speed the motor will start at in automatic mode.")
 #define MOTOR_MAX_SPEED_HELP _HELPSTR("Maximum speed for the motor in auto-ramp mode.")
-#define MOTOR_RAMP_TIME_S_HELP _HELPSTR("The time it takes for the motor to reach `motor_max_speed` in auto ramp mode.")
+#define MOTOR_RAMP_TIME_S_HELP _HELPSTR("The time it takes for the motor to reach Motor Max Speed in automatic mode.")
 #define EDGE_DELAY_HELP _HELPSTR("Minimum time (ms) after edge detection before resuming stimulation.")
 #define MAX_ADDITIONAL_DELAY_HELP _HELPSTR("Maximum time (ms) that can be added to the edge delay before resuming stimulation. A random number will be picked between 0 and this setting each cycle. 0 to disable.")
 #define MINIMUM_ON_TIME_HELP _HELPSTR("Time (ms) after stimulation starts before edge detection is resumed.")
@@ -41,20 +41,20 @@ extern "C" {
 #define SCREEN_TIMEOUT_SECONDS_HELP _HELPSTR("Time, in seconds, before the screen turns off. 0 to disable.")
 #define PRESSURE_SMOOTHING_HELP _HELPSTR("Number of samples to take an average of. Higher results in lag and lower resolution!")
 #define CLASSIC_SERIAL_HELP _HELPSTR("Output continuous stream of arousal data over serial for backwards compatibility with other software.")
-#define SENSITIVITY_THRESHOLD_HELP _HELPSTR("The arousal threshold for orgasm detection. Lower = sooner cutoff.")
+#define SENSITIVITY_THRESHOLD_HELP _HELPSTR("The arousal threshold for orgasm detection. Lower values stop sooner.")
 #define UPDATE_FREQUENCY_HZ_HELP _HELPSTR("Update frequency for pressure readings and arousal steps. Higher = crash your serial monitor.")
-#define SENSOR_SENSITIVITY_HELP _HELPSTR("Analog pressure prescaling. Adjust this until the pressure is ~60-70%")
+#define SENSOR_SENSITIVITY_HELP _HELPSTR("Analog pressure prescaling. Please see instruction manual.")
 #define USE_AVERAGE_VALUES_HELP _HELPSTR("Use average values when calculating arousal. This smooths noisy data.")
 #define VIBRATION_MODE_HELP _HELPSTR("Vibration Mode for main vibrator control.")
 #define USE_POST_ORGASM_HELP _HELPSTR("Use post-orgasm torture mode and functionality.")
-#define CLENCH_PRESSURE_SENSITIVITY_HELP _HELPSTR("Threshold over arousal to detect a clench : Lower values increase sensitivity")
-#define CLENCH_THRESHOLD_2_ORGASM_HELP _HELPSTR("Threshold variable that is tick counts of clench to detect orgasm")
-#define CLENCH_DETECTOR_IN_EDGING_HELP _HELPSTR("Use the clench detector to adjust Arousal")
-#define AUTO_EDGING_DURATION_MINUTES_HELP _HELPSTR("How long to edge before permiting an orgasm")
-#define POST_ORGASM_DURATION_SECONDS_HELP _HELPSTR("How long to stimulate after orgasm detected")
-#define EDGE_MENU_LOCK_HELP _HELPSTR("Deny access to menu starting in the edging session")
-#define POST_ORGASM_MENU_LOCK_HELP _HELPSTR("Deny access to menu starting after orgasm detected")
-#define MAX_CLENCH_DURATION_HELP _HELPSTR("Duration the clench detector can raise arousal if clench detector turned on in edging session")
+#define CLENCH_PRESSURE_SENSITIVITY_HELP _HELPSTR("Minimum additional Arousal level to detect clench. See manual.")
+#define CLENCH_THRESHOLD_2_ORGASM_HELP _HELPSTR("Threshold variable that is tick counts of clench to detect orgasm. See manual.")
+#define CLENCH_DETECTOR_IN_EDGING_HELP _HELPSTR("Use the clench detector to adjust Arousal. See manual.")
+#define AUTO_EDGING_DURATION_MINUTES_HELP _HELPSTR("How long to edge before permiting an orgasm.")
+#define POST_ORGASM_DURATION_SECONDS_HELP _HELPSTR("How long to stimulate after orgasm detected.")
+#define EDGE_MENU_LOCK_HELP _HELPSTR("Deny access to menu starting in the edging session.")
+#define POST_ORGASM_MENU_LOCK_HELP _HELPSTR("Deny access to menu starting after orgasm detected.")
+#define MAX_CLENCH_DURATION_HELP _HELPSTR("Duration the clench detector can raise arousal if clench detector turned on in edging session.")
 
 #ifdef __cplusplus
 }
