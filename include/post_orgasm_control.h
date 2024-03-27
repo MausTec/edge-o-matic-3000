0616ba95d2a0f63502da62e753eaf81f67f03043
Mau Abata <mauabata@gmail.com>
2024-03-27 10:45:19 -0500

    Add maintenance note to post_orgasm_mode.h

--- include/post_orgasm_control.h
+++ include/post_orgasm_control.h
@@ -1,11 +1,19 @@
-#ifndef __edge_o_matic__post_orgasm_control_h
-#define __edge_o_matic__post_orgasm_control_h
+/**
+ * @file post_orgasm_control.h
+ * @brief Routines specific to Post-Orgasm and Overstimulation modes.
+ * @date 2024-03-27
+ *
+ * All code related to post-orgasm control should be migrated to this file to reduce the maintenance
+ * burden of orgasm_control. Additionally, any references to "torture" mode should be renamed to
+ * "overstimulation" mode.
+ */
+
+#ifndef __post_orgasm_control_h
+#define __post_orgasm_control_h
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
 
 #ifdef __cplusplus
 }
