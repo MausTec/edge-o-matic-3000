a40541bd71ca85074607b40e4c44e8f215f658fa
B4ben-69 <savard.benoit@gmail.com>
2024-03-29 09:10:47 -0400

    Max speed correctly set

--- src/orgasm_control.c
+++ src/orgasm_control.c
@@ -351,7 +351,7 @@ static void orgasm_control_updateEdgingTime() { // Edging+Orgasm timer
                 ) {
                 update_check(output_state.motor_speed, output_state.motor_speed + output_state.motor_increment);
             } else {
-                update_check(output_state.motor_speed, output_state.motor_speed + output_state.motor_increment);
+                update_check(output_state.motor_speed, Config.motor_max_speed );
             }
         } else {                                // Post_orgasm timer reached
             if (output_state.motor_speed > 0) { // Ramp down motor speed to 0
